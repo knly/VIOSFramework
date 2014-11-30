57ccc455299bc554ec1a5185ebc4b72a66f3d0c1
Nils Fischer <n.fischer@viwid.com>
2014-11-30 12:39:09 +0100

    implemented on-disc caching

--- Views/CachedTileOverlay/CachedTileOverlay.swift
+++ Views/CachedTileOverlay/CachedTileOverlay.swift
@@ -12,34 +12,85 @@ import VILogKit
 
 class CachedTileOverlay: MKTileOverlay {
    
-    let cache = NSCache()
     let operationQueue = NSOperationQueue()
     
+    let cacheIdentifier: String
+    
+    init(URLTemplate: String, cacheIdentifier: String) {
+        self.cacheIdentifier = cacheIdentifier
+        super.init(URLTemplate: URLTemplate)
+    }
+    
     override func loadTileAtPath(path: MKTileOverlayPath, result: ((NSData!, NSError!) -> ())!) {
-        // FIXME: caching does not work
         if let result = result {
-            if let cachedData = self.cache.objectForKey(self.URLForTilePath(path).absoluteString!) as? NSData {
-                logger.log("Loaded cached tile for path (\(path.z),\(path.x),\(path.y))", forLevel: .Verbose)
+            let url = self.URLForTilePath(path)
+            logger.log("Loading tile for path \(path) from URL \(url)...", forLevel: .Verbose)
+            if let cachedData = cachedTileAtPath(path) {
+                logger.log("Loaded cached tile for path \(path).", forLevel: .Debug)
                 result(cachedData, nil)
             } else {
-                let url = self.URLForTilePath(path)
                 let request = NSURLRequest(URL: url)
-                logger.log("Requesting tile for path (\(path.z),\(path.x),\(path.y)) from URL \(url)...", forLevel: .Verbose)
                 NSURLConnection.sendAsynchronousRequest(request, queue: operationQueue, completionHandler: { response, data, error in
-                    var imageData: NSData! = nil
-                    if UIImage(data: data) == nil { // TODO: more efficiently check for error in response
-                        self.logger.log("Could not load tile for path (\(path.z),\(path.x),\(path.y)) from URL \(url).", forLevel: .Verbose)
+                    if data == nil || UIImage(data: data) == nil { // TODO: more efficiently check for error in response
+                        self.logger.log("Could not load tile for path \(path) from URL \(url).", forLevel: .Verbose)
                     } else {
-                        imageData = data
-                        self.cache.setObject(imageData, forKey: url.absoluteString!)
-                        self.logger.log("Loaded and cached tile for path (\(path.z),\(path.x),\(path.y)).", forLevel: .Debug)
+                        self.cacheTile(data, atPath: path)
+                        self.logger.log("Loaded tile for path \(path).", forLevel: .Verbose)
                     }
-                    result(imageData, error)
+                    result(data, error)
                 })
             }
         }
     }
+    
+    func cachedTileAtPath(path: MKTileOverlayPath) -> NSData? {
+        if let cachedFileURL = cachedFileURLForTileAtPath(path) {
+            return NSData(contentsOfURL: cachedFileURL)
+        } else {
+            return nil
+        }
+    }
+    
+    func cacheTile(data: NSData, atPath path: MKTileOverlayPath) {
+        if let cachedFileURL = cachedFileURLForTileAtPath(path) {
+            if let directoryPath = cachedFileURL.URLByDeletingLastPathComponent?.path {
+                var createDirectoriesError: NSError?
+                if NSFileManager.defaultManager().createDirectoryAtPath(directoryPath, withIntermediateDirectories: true, attributes: nil, error: &createDirectoriesError) {
+                    if let cachedFilePath = cachedFileURL.path {
+                        var writeError: NSError?
+                        if NSFileManager.defaultManager().createFileAtPath(cachedFilePath, contents: data, attributes: nil) {
+                            self.logger.log("Cached tile for path \(path) at \(cachedFilePath).", forLevel: .Verbose)
+                        } else {
+                            self.logger.log("Failed to cache tile at path \(path) at \(cachedFilePath) with error: \(writeError)", forLevel: .Warning)
+                        }
+                    } else {
+                        self.logger.log("Failed to cache tile at path \(path) at \(cachedFileURL).", forLevel: .Warning)
+                    }
+                } else {
+                    self.logger.log("Failed to create cache directory for tile at path \(path) at \(directoryPath) with error: \(createDirectoriesError)", forLevel: .Warning)
+                }
+            } else {
+                self.logger.log("Failed to create cache directory for tile at path \(path) at \(cachedFileURL).", forLevel: .Warning)
+            }
+        }
+    }
+    
+    func cachedFileURLForTileAtPath(path: MKTileOverlayPath) -> NSURL? {
+        if let cachesDirectory = NSFileManager.defaultManager().URLForDirectory(.CachesDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true, error: nil) {
+            return NSURL(string: "map-tiles/\(cacheIdentifier)/\(path.z)/\(path.x)/\(path.y).png", relativeToURL: cachesDirectory)
+        } else {
+            return nil
+        }
+    }
 
+}
+
+extension MKTileOverlayPath: Printable {
+    
+    public var description: String {
+        return "(\(self.z),\(self.x),\(self.y))"
+    }
+    
 }
 
 
